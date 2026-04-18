42ea562cc48e66cf0e1ad75922248264d5322668
shubham260990 <shubhamgl386@gmail.com>
2026-04-18 13:53:10 +0000

    regex search python script

--- my-python-scripts/search.py
+++ my-python-scripts/search.py
@@ -1,1 +1,11 @@
+import re
 
+text = "My name is Shubham"
+pattern = r"name"
+
+search =  re.search(pattern,text)
+if search:
+    print ("Match found:",search.group())
+else:
+    print("Match not found")
+    
