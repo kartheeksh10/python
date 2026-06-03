48d55700cb036b25b754e5109dde72ee65fbd12f
lokeshreddy306 <lokeshasi306@gmail.com>
2026-06-03 18:35:47 +0000

    modules and conditions

--- Day-04/advance_cal.py
+++ Day-04/advance_cal.py
@@ -1,3 +1,17 @@
-import cal as base_cal
+my_list = [1, 2, 3, 4, 5]
 
-base_cal.sub()
+# Identity operators
+a = my_list
+b = [1, 2, 3, 4, 5]
+
+c=a is my_list
+d=b is not my_list
+
+# Membership operators
+element_in_list = 3 in my_list
+element_not_in_list = 5 not in my_list
+
+print("a is my_list:", c)
+print("b is not my_list:", d)
+print("3 in my_list:", element_in_list)
+print("6 not in my_list:", element_not_in_list)
--- Day-04/cal.py
+++ Day-04/cal.py
@@ -1,3 +1,6 @@
+import sys
+import os 
+
 def add(num1, num2):
     c = num1 + num2
     return c
@@ -6,6 +9,19 @@ def sub(num1, num2):
     d = num1 - num2
     return d
 
-addition=(add(3, 6))
-print(addition)
-print(sub(4, 2))
+num1 = float(sys.argv[1])
+num2 = float(sys.argv[2])
+operation = sys.argv[3]
+
+if operation == "add":
+    output = (add(num1, num2))
+    print(output)
+
+elif operation == "sub":
+    print(sub(num1, num2))
+
+else:
+    print("invalid opertion")
+
+passwd=os.getenv("password")
+print(passwd)
