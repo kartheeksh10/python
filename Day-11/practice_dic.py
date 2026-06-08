de750e5834adfdacee34929436993b43f58dff2a
lokeshreddy306 <lokeshasi306@gmail.com>
2026-06-08 17:36:20 +0000

    counting no. of pull requests in the github repo

--- Day-11/practice_dic.py
+++ Day-11/practice_dic.py
@@ -3,7 +3,17 @@ import requests
 response = requests.get("https://api.github.com/repos/iam-veeramalla/python-for-devops/pulls")
 
 full_data = response.json()
+pr_count ={}
 #for i in range(len(full_data)): --> this can be used for indexing
     #print(full_data[i]["user"]["login"])
 for i in full_data:
-    print(i["user"]["login"])
+    username=i["user"]["login"]
+    if username in pr_count:
+        pr_count[username] +=1
+    else:
+        pr_count[username] = 1
+    #print(username)
+    #pr_count[username]=pr_count.get(username,0) + 1
+    for user, count in pr_count.items():
+        print(f"{user}:{count}")
+
